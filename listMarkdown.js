59b06471eb6eb2ac16756c748faa759233741035
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-05-15 18:55:01 -0500

    Hago el fetch e imprimo la data en formato json

--- listMarkdown.js
+++ listMarkdown.js
@@ -2,4 +2,9 @@ function listMarkdown () {
 	const url = 'http://localhost:3000/contenido';
 	let listDiv = document.querySelector('#list');
 
+	fetch(url).then(
+		(response) => {return response.json();}
+	).then(
+		(data) => {console.log(data){
+	);
 }
