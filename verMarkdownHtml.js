03e0db54d5204600903dc3e67b91bfbcc9cd187b
Anthony Joaquin Ticona Hareth <aticonaha@unsa.edu.pe>
2022-05-15 15:59:34 -0500

    Parámetro para la función y  bucle para recorrer el archivo json

--- verMarkdownHtml.js
+++ verMarkdownHtml.js
@@ -1,4 +1,4 @@
-function verMarkdownHtml() {
+function verMarkdownHtml(tituloMarkdown) {
     const url = 'http://localhost:3000/contenido';
     var contenidoDiv = document.querySelector('#contenido');
     var textoMarkdown = "";  //Aquí se almacenará el contenido Markdown
@@ -9,4 +9,12 @@ function verMarkdownHtml() {
             markdownJson = response.json()
         }
     );
+
+    //Bucle que recorra en el array json para almacenar el contenido respecto al título
+    for(let i = 0; i < markdownJson.texts.length; i++) {
+        if(tituloMarkdown == markdownJson.texts[i].title) {
+            textoMarkdown = markdownJson.texts[i].text;
+        }
+    }
+    contenidoDiv.innerHTML = textoMarkdown;
 }
