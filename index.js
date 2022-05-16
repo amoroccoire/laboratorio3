46e6b54a3d05896da7e8e408cebe99a78d03b3df
Anthony Joaquin Ticona Hareth <aticonaha@unsa.edu.pe>
2022-05-16 18:03:51 -0500

    index.js (gets para mi función) creado

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bp = require('body-parser');
+const { response } = require('express');
+const MarkdownIt = require('markdown-it'),
+    md = new MarkdownIt()
+const app = express();
+
+app.use(express.static('priv'))
+app.use(bp.json())
+app.use(bp.urlencoded({
+    extended: true
+}))
+
+app.listen(3000, () => {
+	console.log('Escuchando en: http://localhost:3000')
+});
+
+app.get('/', (request, response) => {
+	response.sendFile(path.resolve(__dirname, 'index.html'))
+});
