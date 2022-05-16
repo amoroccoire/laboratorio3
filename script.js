4b31c237b9a49bda694ae40812e6776b9d91e93c
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2022-05-16 01:17:38 -0500

    agregando el contenido adicional de otra rama para no generar conflictos

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
+const MarkdownIt = require('markdown-it'),
+    md = new MarkdownIt();
+
+add.use(express.static('priv'));
 app.use(express.static('pub'));
 
 app.listen(3000, () => {
@@ -10,4 +15,22 @@ app.listen(3000, () => {
 
 app.get('/', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.get('/contenido', (request, response) => {
+	response.header('Content-Type', 'application/json');
+	response.sendFile(path.resolve(__dirname, 'priv/contenido.json'));
+});
+
+app.get('/listMarkdown.js', (request, response) => {
+	response.sendFile(path.resolve(__dirname, 'listMarkdown.js'))
+});
+
+app.post('/markdown', (request, response) => {
+	let markDownText = request.body.text;
+	let htmlText = md.render(markDownText);
+	response.setHeader('Content-Type', 'application/json');
+	response.end(JSON.stringify({
+		markdown: htmlText
+	}))
 });
